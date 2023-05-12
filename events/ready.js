83186b94a7e566d010989e5f3301ca78d89c1455
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2023-05-12 20:58:26 +0000

    Added MGT & Bug Fixes

--- events/ready.js
+++ events/ready.js
@@ -7,7 +7,5 @@ module.exports = {
   once: true,
   async execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}!`);
-    if(!fs.existsSync(__dirname + "/../nohup.out")) return;
-    fs.writeFile(__dirname + "/../nohup.out", '', function (err) { if (err) throw err });
   },
 };
