759dd47937b6988ae94ba4b0a423fd271182c7d5
ARMN88 <56323614+ARMN88@users.noreply.github.com>
2023-05-13 02:34:07 -0500

    Updated Info Command

--- commands/info.js
+++ commands/info.js
@@ -7,7 +7,15 @@ module.exports = {
     .setDescription('Get information about this server.'),
     async execute(interaction) {
         const infoEmbed = new EmbedBuilder()
-            .setDescription("info");
+            .setTitle(`${interaction.guild.name} Info`)
+            .setColor(0x1c73ba)
+            .setImage(interaction.guild.iconURL({size: 512}))
+            .addFields(
+                { name: "Member Count", value: `${interaction.guild.memberCount}` },
+                { name: "Created At", value: `<t:${interaction.guild.createdTimestamp}:R>` },
+                { name: "Created By", value: `<@${interaction.guild.ownerId}>` }
+            );
+
         return await interaction.reply({ embeds: [infoEmbed] });
     },
 };
